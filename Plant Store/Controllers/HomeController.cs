76b94cb681d4e77d95276a93962e93da06e519d7
Anastasia <anastasiya.domina.2015@gmail.com>
2021-12-10 23:23:47 +0200

    Main Page user design is DONE

--- Plant Store/Controllers/HomeController.cs
+++ Plant Store/Controllers/HomeController.cs
@@ -17,11 +17,13 @@ namespace Plant_Store.Controllers
     {
         private readonly IPlant plantRepository;
         private readonly IPlantCategory categoryRepository;
+        private readonly DataContext db;
 
-        public HomeController(IPlant plantRepository, IPlantCategory categoryRepository)
+        public HomeController(IPlant plantRepository, IPlantCategory categoryRepository, DataContext context)
         {
             this.plantRepository = plantRepository;
             this.categoryRepository = categoryRepository;
+            db = context;
         }
 
         public ActionResult View(int id)
@@ -31,6 +33,52 @@ namespace Plant_Store.Controllers
             {
                 return View(plant);
             }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var plant = plantRepository.Plants.FirstOrDefault(i => i.PlantId == id);
+            SelectList cat = new SelectList(db.Category, "CategoryId", "Name", plant.CategoryId);
+            ViewBag.Cat = cat;
+            if (plant != null)
+            {
+                return View(plant);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Plant plant)
+        {
+            db.Entry(plant).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            SelectList cat = new SelectList(db.Category, "CategoryId", "Name");
+            ViewBag.Cat = cat;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Plant plant)
+        {
+            db.Entry(plant).State = EntityState.Added;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int? id)
+        {
+            var plant = plantRepository.Plants.FirstOrDefault(i => i.PlantId == id);
+            db.Plant.Remove(plant);
+            db.SaveChanges();
             return RedirectToAction("Index");
         }
 
