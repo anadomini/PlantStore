5ca8bbde34ae8af1257f5d2de205f100ada8d9e2
Anastasia <anastasiya.domina.2015@gmail.com>
2021-12-12 21:05:02 +0200

    Controller name changed

--- Plant Store/Controllers/RoleController.cs
+++ Plant Store/Controllers/RoleController.cs
@@ -9,12 +9,12 @@ using System.Threading.Tasks;
 
 namespace Plant_Store.Controllers
 {
-    public class RolesController : Controller
+    public class RoleController : Controller
     {
         RoleManager<IdentityRole> _roleManager;
         UserManager<User> _userManager;
 
-        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
         {
             _roleManager = roleManager;
             _userManager = userManager;
